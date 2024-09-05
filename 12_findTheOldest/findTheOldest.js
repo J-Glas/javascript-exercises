7136282f614f49e7eec8185ca9c98c74ccca18d0
SirGallahad655 <glasbrennerjens@posteo.de>
2024-09-05 13:58:05 +0200

    completed all javascript exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
+  return array.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
+//   return array.forEach((people) => {
+//     var ageOfPerson = people.yearOfDeath - people.yearOfBirth;
+//     var oldestPerson = people.sort((a, b) => b.ageOfPerson - a.ageOfPerson);
+//     return oldestPerson;
+//   });
+// };
 
 // Do not edit below this line
 module.exports = findTheOldest;
